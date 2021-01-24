0f3d0b964ddf576c6f4108eadec046e6d3590261
John Zaras <48223149+sinnec@users.noreply.github.com>
2021-01-24 11:25:19 +0200

    Add files via upload

---
+++ Canadian SIN Validity Check.py
@@ -0,0 +1,18 @@
+sin = input("Enter SIN to check:\n")
+total = 0
+for i in range(len(sin)):
+    if i % 2 != 0:
+        double = int(sin[i]) * 2
+        if len(str(double)) == 2:
+            total += int(str(double)[0]) + int(str(double)[1])
+        else:
+            total += double
+    else:
+        total += int(sin[i])
+        
+if total % 10 == 0:
+    print(f"SIN {sin} is valid!")
+else:
+    print(f"SIN {sin} is NOT valid!")
+
+input()
